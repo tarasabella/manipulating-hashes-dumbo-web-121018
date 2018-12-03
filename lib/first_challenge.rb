fe1c7b30b73ee26f7208d99cc1f3c979b6db8151
Tara Sabella <tarasabella@gmail.com>
2018-12-03 03:47:56 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,13 +15,15 @@ def first_challenge
 
   #your code here
 
- contacts["Freddy Mercury"].each do |attribute, value|
-      if attribute == :favorite_icecream_flavors
-        value.delete_if do |flavor|
-          flavor == "strawberry"
+contacts["Freddy Mercury"].each |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if do |flavor| flavor == "strawberry"
+      end
       end
     end
-  end
-  #remember to return your newly altered contacts hash!
-  contacts
-end
+puts contacts
+ 
+ 
+ 
+ 
+ 
